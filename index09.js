7811d8b3b177f32b0bab3a0c0c764322d1e5bfee
Dmytro Petyshyn <dev6012@gmail.com>
2023-03-28 21:46:58 +0300

    gg

--- index09.js
+++ index09.js
@@ -8,14 +8,29 @@
 //     { name: 'Sapphire', price: 1400, quantity: 7 },
 //     { name: 'Ruby', price: 800, quantity: 2 },
 //   ],
+
+// VARIANT 2
+// calcTotalPrice(stoneName) {
+//   let stoneTotalPrice = 0;
+//   this.stones.forEach(stone => {
+//     if (stone.name === stoneName) {
+//       stoneTotalPrice = stone.price * stone.quantity;
+//     }
+//   });
+//   return stoneTotalPrice;
+// },
+
+// OTHER VARIANT
+// calcTotalPrice(stoneName) {
+//   const currentStone = this.stones.find(({ name }) => name === stoneName);
+//   console.log(currentStone);
+//   return currentStone.price * currentStone.quantity;
+// },
+
+// OTHER SHORTER VARIANT
 //   calcTotalPrice(stoneName) {
-//     let stoneTotalPrice = 0;
-//     this.stones.forEach(stone => {
-//       if (stone.name === stoneName) {
-//         stoneTotalPrice = stone.price * stone.quantity;
-//       }
-//     });
-//     return stoneTotalPrice
+//     const {price, quantity} = this.stones.find(({ name }) => name === stoneName);
+//     return price * quantity;
 //   },
 // };
 
@@ -59,7 +74,7 @@
 //     email: 'poly@hotmail.com',
 //   })
 // );
-// console.log(phonebook)
+// console.log(phonebook);
 
 // Example 3 - Калькулятор
 // Создайте объект calculator с тремя методами:
@@ -74,15 +89,75 @@
 //     this.b = b;
 //   },
 //   add() {
-//     return this.a + this.b
+//     return this.a + this.b;
 //   },
 //   mult() {
 //     return this.a * this.b;
 //   },
-// }
+// };
 
-// calculator.read(10, 20)
+// calculator.read(10, 20);
 // console.log(calculator.add());
 // console.log(calculator.mult());
 
-// console.log(calculator)
+// console.log(calculator);
+//
+//
+//RAZBIRAEMSA
+//
+// const user = {
+//   username: 'Alex',
+//   sayHi() {
+//     console.log(`Hello ${this.username}`);
+//   },
+// };
+// const user2 = {
+//   username: 'Olga',
+//   foo: user.sayHi,
+// };
+
+// user.sayHi();
+// user2.foo();
+//
+//
+//
+//
+// function boo() {
+//   console.log(this);
+// }
+
+// const user = {
+//   username: 'Alex',
+//   sayHi() {
+//     const foo = () => console.log(this);
+//     // foo.call({ username: 'Lena' });
+//     const fooCopy = foo.bind({ username: 'Lena' });
+//     fooCopy();
+//     // boo();
+//     console.log(`Hello ${this.username}`);
+//   },
+// };
+
+// user.sayHi();
+// // boo.call(user);
+//
+//
+//
+//
+//
+const customer = {
+  firstName: 'Jacob',
+  lastName: 'Mercer',
+  getFullName() {
+    console.log(this);
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+
+function makeMessage(callback) {
+  console.log(callback);
+  // callback() это вызов метода getFullName без объекта
+  console.log(`Обрабатываем заявку от ${callback()}.`);
+}
+
+makeMessage(customer.getFullName.bind(customer)); // Будет ошибка при вызове функции
