f353292be2e0afb33c46820cacab45cbba22249f
Dmytro Petyshyn <dev6012@gmail.com>
2023-04-02 13:08:43 +0300

    ae

---
+++ index10.js
@@ -0,0 +1,212 @@
+// Example 1 - Блоггер
+// Напиши класс Blogger для создания обьекта блоггера со следующим свойствами:
+
+// email - почта, строка
+// age - возраст, число
+// numberOfPosts - кол-во постов, число
+// topics - массив тем на которых специализируется блоггер
+// Класс ожидает один параметр - объект настроек с одноимёнными свойствами.
+
+// Добавь метод getInfo(), который, возвращает строку: User ${почта} is ${возраст} years old and has ${кол-во постов} posts.
+
+// Добавь метод updatePostCount(value), который в параметре value принимает количество постов которые нужно добавить пользователю.
+
+// class Blogger {
+//   constructor({ name, age, numberOfPosts, topics }) {
+//     this.name = name;
+//     this.age = age;
+//     this.numberOfPosts = numberOfPosts;
+//     this.topics = topics;
+//   }
+
+//   getInfo() {
+//     return `User ${this.name} is ${this.age} years old and has ${this.numberOfPosts} posts.`;
+//   }
+
+//   updatePostCount(value) {
+//     this.numberOfPosts += value;
+//   }
+// }
+
+// const mango = new Blogger({
+//   name: 'mango@mail.com',
+//   age: 24,
+//   numberOfPosts: 20,
+//   topics: ['tech', 'cooking'],
+// });
+// console.log(mango);
+// console.log(mango.getInfo()); // User mango@mail.com is 24 years old and has 20 posts
+// mango.updatePostCount(5);
+// console.log(mango.getInfo()); // User mango@mail.com is 24 years old and has 25 posts
+// console.log(mango);
+
+// const poly = new User({
+//   name: 'poly@mail.com',
+//   age: 19,
+//   numberOfPosts: 17,
+//   topics: ['sports', 'gaming', 'health'],
+// });
+// console.log(poly.getInfo()); // User poly@mail.com is 19 years old and has 17 posts
+// poly.updatePostCount(4);
+// console.log(poly.getInfo()); // User poly@mail.com is 19 years old and has 21 posts
+//
+//
+//
+//
+//
+//
+//
+//Example 2 - Хранилище
+// Напиши класс Storage который создаёт объекты для управления складом товаров. При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+
+// Добавь методы класса:
+
+// getItems() - возвращает массив товаров.
+// addItem(item) - получает новый товар и добавляет его к текущим.
+// removeItem(item) - получает товар и, если он есть, удаляет его из текущих.
+
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+
+//   getItems() {
+//     return this.items;
+//   }
+
+//   addItem(item) {
+//     this.items.push(item);
+//   }
+
+//   removeItem(item) {
+//     this.items = this.items.filter((el) => el !== item)
+//   };
+// }
+
+// const storage = new Storage(['🍎', '🍋', '🍇', '🍑']);
+
+// const items = storage.getItems();
+// console.table(items); // [ '🍎', '🍋', '🍇', '🍑' ]
+
+// storage.addItem('🍌');
+// console.table(storage.items); // [ '🍎', '🍋', '🍇', '🍑', '🍌' ]
+
+// storage.removeItem('🍋');
+// console.table(storage.items); // [ '🍎', '🍇', '🍑', '🍌' ]
+//
+//
+///
+//
+//
+//
+//
+//
+// Example 3 - User
+// Напиши класс User который создаёт объект со свойствами login и email. Объяви приватные свойства #login и #email, доступ к которым сделай через геттер и сеттер login и email.
+
+// class User {
+//   #login;
+//   #email;
+//   constructor({ login, email }) {
+//     this.#login = login;
+//     this.#email = email;
+//   }
+//   get login() {
+//     return this.#login;
+//   }
+//   set login(newLogin) {
+//     return (this.#login = newLogin);
+//   }
+
+// get email() {
+//     return this.#email;
+//   }
+//   set email(newEmail) {
+//     return (this.#email = newEmail);
+//   }
+// }
+
+// const mango = new User({
+//   login: 'Mango',
+//   email: 'mango@dog.woof',
+// });
+
+// console.log(mango.login); // Mango
+// mango.login = 'Mangodoge';
+// console.log(mango.login); // Mangodoge
+
+// const poly = new User({
+//   login: 'Poly',
+//   email: 'poly@mail.com',
+// });
+
+// console.log(poly.login); // Poly
+// poly.login = 'Polycutie';
+// console.log(poly.login); // Polycutie
+
+//
+//
+//
+//
+//
+//
+//
+//
+
+// Example 4 - Заметки
+// Напиши класс Notes который управляет коллекцией заметок в свойстве items. Заметка это объект со свойствами text и priority. Добавь классу статическое свойство Priority, в котором будет храниться объект с приоритетами.
+
+// {
+//   LOW: 'low',
+//   NORMAL: 'normal',
+//   HIGH: 'high'
+// }
+// Добавь методы addNote(note), removeNote(text) и updatePriority(text, newPriority).
+
+class Notes {
+  static Priority = {
+    LOW: 'low',
+    NORMAL: 'normal',
+    HIGH: 'high',
+  };
+
+  constructor(items = []) {
+    this.items = items;
+  }
+
+  addNote(note) {
+    this.items.push(note);
+  }
+
+  removeNote(text) {
+    // const index = this.items.findIndex(({ text: noteText }) => text === noteText)
+    // this.items.splice(index, 1)
+
+    this.items = this.items.filter(({ text: noteText }) => text !== noteText);
+  }
+
+  updateNote(text, newPriority) {
+    const targetObj = this.items.find(
+      ({ text: noteText }) => text === noteText
+    );
+    console.log(targetObj);
+    targetObj.priority = newPriority;
+  }
+}
+
+const myNotes = new Notes([]);
+
+myNotes.addNote({ text: 'Моя первая заметка', priority: Notes.Priority.LOW });
+console.log(myNotes.items);
+
+myNotes.addNote({
+  text: 'Моя вторая заметка',
+  priority: Notes.Priority.NORMAL,
+});
+console.log(myNotes.items);
+
+myNotes.removeNote('Моя первая заметка');
+console.log(myNotes.items);
+
+myNotes.updateNote('Моя вторая заметка', Notes.Priority.HIGH);
+console.log(myNotes.items);
